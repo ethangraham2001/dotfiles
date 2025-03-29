5a2263544103008580a9daf7be790b3af3d23983
Ethan Graham <ethan.w.s.graham@gmail.com>
2025-03-29 12:06:53 +0100

    Add rustfmt formatting for rust files

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -2,6 +2,7 @@ require("conform").setup({
     formatters_by_ft = {
         c = { "clang_format" },
         cpp = { "clang_format" },
+        rust = { "rustfmt" },
     },
     format_on_save = true,
 })
