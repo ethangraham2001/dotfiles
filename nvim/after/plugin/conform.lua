0292764fcd646d1cd56a84fa5c16a4cf1608af98
Ethan Graham <ethan.w.s.graham@gmail.com>
2025-03-31 18:15:10 +0200

    nvim: add terraform to conform formatting list

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -3,6 +3,7 @@ require("conform").setup({
         c = { "clang_format" },
         cpp = { "clang_format" },
         rust = { "rustfmt" },
+        terraform = { "terraform_fmt" },
     },
     format_on_save = true,
 })
