d1e130c0bfd3b0f6cbbb60bb53873f72f3c2220f
Ethan Graham <ethan.w.s.graham@gmail.com>
2025-04-10 14:44:53 +0200

    Add indent enabled to treesitter, update remap for diagnostics

--- nvim/lua/ethan/remap.lua
+++ nvim/lua/ethan/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", opts);
 vim.keymap.set("n", "<leader>d", ':lua vim.diagnostic.open_float(0, {scope="line"})<cr>')
-vim.keymap.set('n', '<leader>dd', ':Telescope diagnostics bufnr=0<cr>')
+vim.keymap.set('n', '<leader>td', ':Telescope diagnostics bufnr=0<cr>')
 vim.keymap.set("n", "<leader>pwd", ":lua print(vim.fn.expand(\"%:p:h\"))<CR>", opts)
 vim.keymap.set('n', '<leader>ne', vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 vim.keymap.set('n', '<leader>pe', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
