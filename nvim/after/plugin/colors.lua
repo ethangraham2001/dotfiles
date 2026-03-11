c45d40c762889ad19dded24ebb511de33fa8cc37
Ethan Graham <ethan.w.s.graham@gmail.com>
2026-03-11 13:41:55 +0100

    nvim/colors: make default colorscheme rose-pine-moon, format file

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,10 @@
 function TransparentBackground(should_be_transparent)
-	-- color = color or 'gruvbox'
-	-- vim.cmd.colorscheme(color)
+    -- color = color or 'gruvbox'
+    -- vim.cmd.colorscheme(color)
 
     if should_be_transparent then
-        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 end
 
@@ -12,7 +12,7 @@ require('gruvbox').setup({
     contrast = "hard",
 })
 
-vim.cmd[[
+vim.cmd [[
   augroup SpaceDuckOverrides
     autocmd!
     autocmd ColorScheme spaceduck highlight Comment guifg=#8888AA gui=italic
@@ -20,7 +20,7 @@ vim.cmd[[
 ]]
 
 -- default colorscheme
-vim.cmd('colorscheme tokyonight-moon');
+vim.cmd('colorscheme rose-pine-moon');
 
 function ColorScheme(scheme_name)
     vim.cmd('colorscheme ' .. scheme_name)
@@ -36,22 +36,23 @@ function Darkify()
     ]])
 end
 
-local color_scheme_names = {'spaceduck', 'newpaper', 'carbonfox', 'monokai-pro'}
+local color_scheme_names = { 'spaceduck', 'newpaper', 'carbonfox', 'monokai-pro' }
 local curr_scheme_idx = 1
 
 function NextColorScheme()
     curr_scheme_idx = curr_scheme_idx + 1
-    if curr_scheme_idx > #color_scheme_names then 
+    if curr_scheme_idx > #color_scheme_names then
         curr_scheme_idx = 1
     end
 
     local nextScheme = color_scheme_names[curr_scheme_idx]
     ColorScheme(nextScheme)
 end
+
 vim.keymap.set("n", "<leader>nc", ":lua NextColorScheme()<cr>")
 
 function AOS()
-	vim.cmd('colorscheme monokai-pro-classic')
-	Darkify()
-	vim.cmd('set expandtab')
+    vim.cmd('colorscheme monokai-pro-classic')
+    Darkify()
+    vim.cmd('set expandtab')
 end
