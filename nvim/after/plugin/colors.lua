9558181552bf1e858faae76051456dbdc8f8f8b7
Ethan Graham <ethan.w.s.graham@gmail.com>
2026-03-11 15:09:54 +0100

    kitty+nvim: revert to tokyo night theme because ragequit

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -20,7 +20,7 @@ vim.cmd [[
 ]]
 
 -- default colorscheme
-vim.cmd('colorscheme rose-pine-moon');
+vim.cmd('colorscheme tokyonight-moon');
 
 function ColorScheme(scheme_name)
     vim.cmd('colorscheme ' .. scheme_name)
