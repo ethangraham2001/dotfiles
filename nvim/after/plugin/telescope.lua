5999a92189c53dc2daba1c9d21dc1c5e9059c56c
Ethan Graham <ethan.w.s.graham@gmail.com>
2026-04-14 15:38:01 +0200

    nvim: add find-marks binding in telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) 	-- search files with telescope
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- search files with telescope
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>gg', builtin.git_files, {}) 	-- search git files
-vim.keymap.set('n', '<leader>oo', builtin.oldfiles, {}) 	-- search git files
+vim.keymap.set('n', '<leader>gg', builtin.git_files, {})  -- search git files
+vim.keymap.set('n', '<leader>oo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 
-vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
